ac8a6bb95d7cc772e04260cfa9f7bd16aa4e8940
Yevgeniya-Lat <e9618281262@gmail.com>
2022-02-28 23:07:11 +0300

    Commit example

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число,    ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число,    ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число,    ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+    if  (B > max) max = B;
+    if (C > max) max = C;
+
+Console.Write($"Максимальное число из введенных {max}");
